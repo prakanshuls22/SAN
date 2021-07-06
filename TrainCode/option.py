b0c79fa4ee8fcfcbcab3005b8e4d3ab62d202f54
Prakanshul Saxena <sheenusaxena2211@gmail.com>
2021-07-06 14:59:55 +0530

    dir_data updated

--- TrainCode/option.py
+++ TrainCode/option.py
@@ -21,7 +21,7 @@ parser.add_argument('--seed', type=int, default=1,
                     help='random seed')
 
 # Data specifications
-parser.add_argument('--dir_data', type=str, default='/data/daitao/code_142/superresolution/SR_dataset',
+parser.add_argument('--dir_data', type=str, default='/scratch/ssrivastava.cse18.iitbhu',
                     help='dataset directory')
 parser.add_argument('--dir_demo', type=str, default='../test',
                     help='demo image directory')
